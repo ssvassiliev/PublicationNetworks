8d25d0f098d5b628d3492c674073fcd6b79bf55b
svassili <svassiliev1@brocku.ca>
2019-01-22 22:55:38 -0400

    drawing graphs

--- make_Network.py
+++ make_Network.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python
 import bibtexparser
+from bibtexparser.bwriter import BibTexWriter
 import networkx as nx
 import jellyfish
 import numpy
-
-bibtex_file = open('data/Zaraiskaya.bib')
-bib_str = bibtex_file.read()
-bib_db = bibtexparser.loads(bib_str)
-
-G = nx.Graph
-threshold = 0.85
+import matplotlib.pyplot as plt
+import os
 
 
 def make_uniq_authors_list(bib_db):
@@ -33,7 +29,8 @@ def make_pairs(allAuthors, threshold):
     for i in range(0, len(allAuthors)):
         for j in range(i+1, len(allAuthors)):
             djawi_1 = jellyfish.jaro_winkler(allAuthors[i], allAuthors[j])
-            djawi_2 = jellyfish.jaro_winkler(allAuthors[i].split()[-1], allAuthors[j].split()[-1])
+            djawi_2 = jellyfish.jaro_winkler(allAuthors[i].split()[-1],
+                                             allAuthors[j].split()[-1])
             if djawi_1 > threshold or djawi_2 > threshold:
                 np = np + 1
                 while(1):
@@ -56,6 +53,24 @@ def make_pairs(allAuthors, threshold):
     return(np, pairs)
 
 
+def save_all_pairs(allAuthors, threshold):
+    pairs = []
+    np = 0
+    for i, auth_1 in enumerate(allAuthors):
+        for auth_2 in allAuthors[i+1:-1]:
+            djawi_1 = jellyfish.jaro_winkler(auth_1, auth_2)
+            djawi_2 = jellyfish.jaro_winkler(auth_1.split()[-1],
+                                             auth_2.split()[-1])
+            if djawi_1 > threshold or djawi_2 > threshold:
+                np = np + 1
+                p = (auth_1, auth_2)
+                pairs.append(p)
+    f = open("pairs.txt", "w+")
+    f.write('\n'.join('%s, %s, y' % x for x in pairs))
+    f.close()
+    return(pairs)
+
+
 def rename_authors(bib_db, pairs):
     for entry in bib_db.entries:
         if 'author' not in entry:
@@ -65,10 +80,25 @@ def rename_authors(bib_db, pairs):
     return(pairs)
 
 
-print('')
+# Main program
+infile = raw_input('Enter name of BibTex file:')
+out_graphml = infile.replace(".bib", ".graphml")
+out_bib = infile.replace(".bib", "-edited.bib")
+# Jaro-Winkler threshold
+threshold = 0.85
+# Graph parameters
+maxNodeSize = 200
+maxEdgeWidth = 3
+G = nx.Graph()
+bibtex_file = open(infile)
+bib_str = bibtex_file.read()
+bib_db = bibtexparser.loads(bib_str)
+print('Constructing network from file: ' + infile)
+# Merge duplicate authors
 while(1):
     allAuthors = make_uniq_authors_list(bib_db)
-    print 'Total number of authors = ' + str(len(allAuthors)) + '\n'
+    print 'Total number of authors = ' + str(len(allAuthors))
+    print 'Using Jaro-Winkler algorithm, threshold ' + str(threshold)
     choice = raw_input("Merge duplicate authors (y/n)?")
     print('')
     if choice == 'y':
@@ -80,36 +110,64 @@ while(1):
         break
     else:
         continue
-
+# Save the edited bibtex file
+writer = BibTexWriter()
+bibtex_str = bibtexparser.dumps(bib_db)
+with open(out_bib, 'w') as bibfile:
+    bibfile.write(bibtex_str.encode('utf8'))
+# Make the list of unique authors
 allAuthors = make_uniq_authors_list(bib_db)
-authorWeight = []
+pairs = save_all_pairs(allAuthors, threshold)
+# Find graph edges. and node weights
 msize = len(allAuthors)
 edges = numpy.zeros((msize, msize))
 weights = numpy.zeros(msize)
-
-k = 0
-for i, entry in enumerate(bib_db.entries):
+for entry in bib_db.entries:
     if 'author' not in entry:
         continue
     authors = entry['author'].split(' and ')
     authors = map(unicode.strip, authors)
-    for j, author in enumerate(authors):
-        print('')
-        if author not in authorWeight:
-            authorWeight.append(author)
-            weights[k] = weights[k] + 1
-            k = k + 1
-        else:
-            il = authorWeight.index(author)
-            weights[il] = weights[il] + 1
-        for ii, author_2 in enumerate(authors):
-            print str(author_2) + ' ' + str(authors)
+    for author in authors:
+        il = allAuthors.index(author)
+        weights[il] = weights[il] + 1
+        for author_2 in authors:
             e2 = allAuthors.index(author_2)
             e1 = allAuthors.index(author)
-            edges[e1][e2] = edges[e1][e2] + 1
+            if e1 != e2:
+                edges[e1][e2] = edges[e1][e2] + 1
+# Normalize weights of nodes and edges
+wm = weights.max()
+for i, each_weight in enumerate(weights):
+    weights[i] = each_weight*maxNodeSize/wm
+for i in range(0, msize):
+    G.add_node(i, weight=weights[i], label=allAuthors[i])
+    for j in range(i+1, msize):
+        if edges[i][j] != 0:
+            G.add_edge(i, j, weight=edges[i][j]*maxEdgeWidth/edges.max())
+# Save graph
+nx.write_graphml(G, out_graphml)
 
+# Draw graph
+labels = dict((n, d['label']) for (n, d) in G.nodes(data=True))
+edgewidth = [d['weight'] for (u, v, d) in G.edges(data=True)]
+nodesize = [d['weight'] for (u, d) in G.nodes(data=True)]
+pos = nx.spring_layout(G, iterations=100)
+nx.draw_networkx_nodes(G, pos, node_size=nodesize)
+nx.draw_networkx_edges(G, pos, width=0.5, edge_color='b')
+nx.draw_networkx_labels(G, pos, labels, alpha=1.0, font_size=8,
+                                            font_color='black')
+plt.axis('off')
+plt.show()
 
-
-
-print str(len(allAuthors)) + ' ' + str(len(authorWeight))
-print weights
+# Draw Hub Ego graph
+# from operator import itemgetter
+# node_and_degree = G.degree()
+# (largest_hub, degree) = sorted(node_and_degree, key=itemgetter(1))[-1]
+# Create ego graph of main hub
+# hub_ego = nx.ego_graph(G, largest_hub)
+# Draw graph
+# pos = nx.spring_layout(hub_ego
+# nx.draw(hub_ego, pos, node_color='b', node_size=50, with_labels=False)
+# Draw ego as large and red
+# nx.draw_networkx_nodes(hub_ego, pos, nodelist=[largest_hub], node_size=300, node_color='r')
+# plt.show()
