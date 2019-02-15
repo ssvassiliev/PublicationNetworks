113da57c3acffbf8de358e388909359e5d0066c7
svassili <svassiliev1@brocku.ca>
2019-02-15 11:40:04 -0400

    multithreading

--- get_Scholar.py
+++ get_Scholar.py
@@ -4,8 +4,24 @@ import bibtexparser
 from bibtexparser.bwriter import BibTexWriter
 from bibtexparser.bibdatabase import BibDatabase
 from progress.bar import Bar
+#from Queue import Queue
+#from threading import Thread
+from multiprocessing.dummy import Pool
+import time
+import tqdm
+
+number_publications = 0
+
+# make the Pool of workers
+pool = Pool(10)
+
+
+def download_publications(p):
+    return p.fill()
+
 
 scholar = raw_input('Search Google Scholar: ')
+# scholar = 'bruce balcom'
 outfile = scholar.split()[-1] + '.bib'
 db = BibDatabase()
 authors = []
@@ -30,20 +46,31 @@ else:
 print 'Downloading ' + authors[ind].name + ' publications'
 author = authors[ind].fill()
 db.entries = []
+pub = []
 id = 1
-bar = Bar('Loading', max=len(author.publications))
-for id, p in enumerate(author.publications):
+number_publications = len(author.publications)
+bar = Bar('Loading', max=number_publications)
+start = time.time()
+pb = pool.imap(download_publications, author.publications)
+for i in pb:
     bar.next()
-    pub = p.fill()
-    if 'year' in pub.bib:
-        pub.bib['year'] = str(pub.bib['year'])
-    pub.bib['ENTRYTYPE'] = unicode('article')
-    if 'abstract' in pub.bib:
-        del pub.bib['abstract']
-    sid = "%04d" % id
-    pub.bib['ID'] = str(sid)
-    db.entries.append(pub.bib)
 bar.finish()
+pool.close()
+pool.join()
+pub = list(pb)
+end = time.time()
+print "Total time: " + str(end - start)
+
+for id, publ in enumerate(pub):
+    if 'year' in publ.bib:
+        publ.bib['year'] = str(publ.bib['year'])
+    publ.bib['ENTRYTYPE'] = unicode('article')
+    if 'abstract' in publ.bib:
+        del publ.bib['abstract']
+    sid = "%04d" % id
+    publ.bib['ID'] = str(sid)
+    db.entries.append(publ.bib)
+
 writer = BibTexWriter()
 bibtex_str = bibtexparser.dumps(db)
 with open(outfile, 'w') as bibfile:
